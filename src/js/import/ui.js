4c9dda759bb7b2e05ef6cbec8c01e978502b5ac3
Tim <ktn_96@icloud.com>
2019-03-28 22:21:37 +0200

    one page

--- src/js/import/ui.js
+++ src/js/import/ui.js
@@ -1,4 +1,4 @@
-import '../lib/selectize.min.js';
+// import '../lib/selectize.min.js';
 // import '../lib/maskedinput.js';
 // import modal from 'jquery-modal';
 
@@ -12,3 +12,10 @@ import '../lib/selectize.min.js';
 //   });
 //   return false;
 // });
+
+
+$('.login__step__forgot').on('click', function() {
+  let link = $(this).attr('href');
+  $(this).closest('.login__step').fadeOut();
+  $(link).fadeIn();
+});
